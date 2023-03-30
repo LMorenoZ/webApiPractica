b3d8470ddf9799a9adf919d42059c62bdfc28b8c
luisj <luisj@LUISPC>
2023-03-30 08:06:43 -0600

    Agregar archivos de proyecto.

---
+++ webApiPractica/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using webApiPractica.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Inyección por dependencia del string de conexión al contexto
+builder.Services.AddDbContext<equiposContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("equiposDbConnection"),
+        sqlServerOptionsAction: sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure();
+        }
+    )
+);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
